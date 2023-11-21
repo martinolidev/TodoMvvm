d8a2367ff68faedcaba6e5915d8c991005097afa
Martin Martinez <mtz.martin@proton.me>
2023-11-21 16:47:55 -0600

    Creating the chanegs so the array changes can be detected

--- TodoMvvm/TodoViewModel.swift
+++ TodoMvvm/TodoViewModel.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+class TodoViewModel: ObservableObject {
+    @Published var allMyTasks: [taskInformation] = []
+    
+    private let model = TodoModel()
+    
+    init() {
+        allMyTasks = model.allMyTasks
+    }
+    
+    func addTask(task: taskInformation) {
+        model.addTask(task: task)
+        allMyTasks.append(task)
+    }
+    
+    func removeTask(task: taskInformation) {
+        model.removeTask(task: task)
+        if let index = allMyTasks.firstIndex(where: { $0.name == task.name }) {
+            allMyTasks.remove(at: index)
+        }
+    }
+    
+    func markAsCompleted(task: taskInformation) {
+        if let index = allMyTasks.firstIndex(where: { $0.name == task.name }) {
+            allMyTasks[index].completed = true
+        }
+    }
+}
