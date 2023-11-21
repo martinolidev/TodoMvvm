4a2a6c46f1e44a46036a148e1e3035d6629f3e43
Martin Martinez <mtz.martin@proton.me>
2023-11-21 16:14:56 -0600

    Initial Commit

---
+++ TodoMvvm/TodoMvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoMvvmApp.swift
+//  TodoMvvm
+//
+//  Created by Martin Martinez on 21/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoMvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
