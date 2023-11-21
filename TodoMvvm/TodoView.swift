b4163854f591423699d44f20ab3e57fe8463ebe1
Martin Martinez <mtz.martin@proton.me>
2023-11-21 16:54:27 -0600

    New file for the view, where I'll instance the viewmodel so the view can update the changes

---
+++ TodoMvvm/TodoView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoView.swift
+//  TodoMvvm
+//
+//  Created by Martin Martinez on 21/11/23.
+//
+
+import SwiftUI
+
+struct TodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoView()
+}
