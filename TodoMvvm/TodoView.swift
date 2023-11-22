4e8ad0af5417e4b0ed01e2cde3b0fcc54b84a6d9
Martin Martinez <mtz.martin@proton.me>
2023-11-22 13:19:10 -0600

    Building the UI, I need to use the viewmodel to change the content dynamically

--- TodoMvvm/TodoView.swift
+++ TodoMvvm/TodoView.swift
@@ -9,12 +9,43 @@ import SwiftUI
 
 struct TodoView: View {
     @ObservedObject var viewModel = TodoViewModel()
+    @State var taskName: String = ""
+    @State var taskDescription: String = ""
     
     var body: some View {
         HStack {
-            Text(viewModel.convertDateToString())
-                .font(.title)
+            Text("To-Do App")
                 .bold()
+                .font(.title)
+            Spacer()
+            Text(viewModel.convertDateToString())
+                .font(.title2)
+                .bold()
+        }
+        ScrollView {
+            
+        }
+        
+        HStack {
+            VStack {
+                TextField("Add a new task!", text: $taskName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("Add a description", text: $taskDescription)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            
+            Button {
+                //
+            } label: {
+                RoundedRectangle(cornerRadius: 16)
+                    .frame(width: 70, height: 80)
+                    .overlay(
+                        Image(systemName: "pencil.tip.crop.circle.badge.plus.fill")
+                            .foregroundColor(.white)
+                            .imageScale(.large)
+                            .bold()
+                    )
+            }
         }
     }
 }
